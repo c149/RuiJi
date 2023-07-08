846cd3f5968c32c167abc037ff7574d3abc3215d
Eternal Heart <initdex@gmail.com>
2023-07-08 18:59:11 +0800

    add //

--- src/main/java/com/ruiji/StartApplication.java
+++ src/main/java/com/ruiji/StartApplication.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Slf4j
 public class StartApplication {
     public static void main(String[] args) {
+        //....
         SpringApplication.run(StartApplication.class);
     }
 }
